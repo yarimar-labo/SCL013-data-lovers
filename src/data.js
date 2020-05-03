55d30dbf3f5c373979100cbf87abf91ecd4c71a8
nadiaGincoff <nadia.gincoff@gmail.com>
2020-05-03 12:30:24 -0400

    Devuelve un array ordenado numerica o alfabeticamente al compararlo con su condition seleccionada.

--- src/data.js
+++ src/data.js
@@ -1,9 +1,42 @@
-// estas funciones son de ejemplo
+import data from './data/pokemon/pokemon.js';
 
-export const example = () => {
-  return 'example';
-};
+// Devuelve un array ordenado numerica o alfabeticamente al compararlo con su condition seleccionada.
+export const sortedData = (condition) => {
 
-export const anotherExample = () => {
-  return 'OMG';
-};
+  if (condition === "ascendente" || condition === "descendente") {
+    return arrayOrderedByNumber(condition);
+  }
+  else if (condition === "a-z" || condition === "z-a") {
+    return arrayOrderedByName(condition);
+  }
+}
+
+// Devuelve un array ordenado alfabeticamente
+export const arrayOrderedByName = (condition) => {
+  let sortedArray = data.pokemon.sort(function (a, b) {
+
+    if (condition === "a-z") {
+      // Devuelve un resultado a traves de operador ternario
+      return (a.name > b.name) ? 1 : -1
+    }
+    else if (condition === "z-a") {
+      return (b.name > a.name) ? 1 : -1
+    }
+  });
+  return sortedArray;
+}
+
+// Devuelve un array ordenado numericamente
+export const arrayOrderedByNumber = (condition) => {
+  let sortedArray = data.pokemon.sort(function (p1, p2) {
+
+    if (condition === "ascendente") {
+      return (p1.id - p2.id);
+    }
+    else if (condition === "descendente") {
+      return (p2.id - p1.id);
+    }
+  });
+  return sortedArray;
+}
+
